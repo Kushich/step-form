930294caf4cd7bde36ffce75b60911eac42778a4
Kushich <kushich@outlook.com>
2020-03-04 19:20:31 +0300

    Updates

---
+++ precache-manifest.12dd6344d576e26ee97f55dfc89d4654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0447e51a9dd46ed2827367ac4789296",
+    "url": "/step-form/index.html"
+  },
+  {
+    "revision": "2891454c282a06106b43",
+    "url": "/step-form/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c55e2f36d9d2387778ca",
+    "url": "/step-form/static/js/2.55177f89.chunk.js"
+  },
+  {
+    "revision": "6d1ae0a7201d2aa71f08b67ad045a152",
+    "url": "/step-form/static/js/2.55177f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2891454c282a06106b43",
+    "url": "/step-form/static/js/main.8aad6260.chunk.js"
+  },
+  {
+    "revision": "6761cc5cb9350f798fa0",
+    "url": "/step-form/static/js/runtime-main.b7e0ba4d.js"
+  }
+]);
